704649ede4572e7a785f937c35cdb3db21930307
Aleskey Andreev <alexmilaylia@yandex.ru>
2023-10-30 00:23:06 +0300

    js modal

--- educational-project-TECHNOMART-main/writeus.js
+++ educational-project-TECHNOMART-main/writeus.js
@@ -1,13 +1,16 @@
 const writeUs = document.querySelector(".write-link");
 const writePopup = document.querySelector(".modal-write");
 const writeClose = writePopup.querySelector(".modal-close");
+const writeName = writePopup.querySelector("[name=name]");
 
 writeUs.addEventListener("click", function (evt) {
   evt.preventDefault();
   writePopup.classList.add("modal-show");
+  writeName.focus();
 });
 
 writeClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   writePopup.classList.remove("modal-show");
 });
+
